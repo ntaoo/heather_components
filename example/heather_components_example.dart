d0f92c0e57e506f4bf63bb8a3a6daa7556123247
ntaoo <ntaoo.g@gmail.com>
2017-01-16 04:08:53 +0900

    More remove unused code.

--- example/heather_components_example.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2017, ntaoo. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-import 'package:heather_components/heather_components.dart';
-
-main() {
-  var awesome = new Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
